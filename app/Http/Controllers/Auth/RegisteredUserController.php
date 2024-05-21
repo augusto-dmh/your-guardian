eee61cd1576130c03ccd0ea90fce3abae3b81462
= <augustodemeloh@protonmail.com>
2024-05-21 12:43:06 -0300

    feat: authentication and mailing
    The default authentication and mailing system has been changed to adapt to the needs of the system (like setting mailgun and setting new user fields in the forms).

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,14 +30,18 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'birthdate' => ['required', 'date'],
+            'password' => ['bail', 'required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
+            'birthdate' => $request->birthdate,
             'password' => Hash::make($request->password),
         ]);
 
