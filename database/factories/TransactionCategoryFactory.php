5dc96b867fc9713e88932f503f6abb926866f58d
= <augustodemeloh@protonmail.com>
2024-05-24 18:35:55 -0300

    feat: transactionCategories seeding
    seeding transaction categories available for testing - the route for handling the user's money is being developed.

---
+++ database/factories/TransactionCategoryFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class TransactionCategoryFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
