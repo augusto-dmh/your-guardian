5237bc41f8ef80ad802730a69a305c8d7c73bd52
= <augustodemeloh@protonmail.com>
2024-05-27 00:39:25 -0300

    refactor: user not having to verify email on development
    There's no need to make a user verify their email while i'm on development; this was useful before when i was testing and configuring mailing but now it just delays testing other features.

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Notifications\ResetPasswordNotification;
 
-class User extends Authenticatable implements MustVerifyEmail
+// class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
