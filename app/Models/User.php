2f97e8bce1a587aa68f324366036d1eb0e4e9447
= <augustodemeloh@protonmail.com>
2024-05-27 00:31:19 -0300

    refactor: notifications table replaced with built-in table
    laravel makes available through "php artisan notifications:table" a notifications table ready to be used aside with laravel's notification built-in structure, so there's no need to define a completely new table since the one generated by the command is enough and even better - it's ready for polymorphic relationships; for associating notifications to more than onde table row.

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,11 +69,6 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->hasMany(Bill::class);
     }
 
-    public function notifications()
-    {
-        return $this->hasMany(Notification::class);
-    }
-
     public function tasks()
     {
         return $this->hasMany(Task::class);
