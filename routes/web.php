a78ee8a2b011af5c88fad2a8f4fe45dcf9698c6d
= <augustodemeloh@protonmail.com>
2024-05-28 00:34:44 -0300

    feat: transactions routes
    transactions related routes (following the standard convention create, store, index, edit, show, update, destroy; except for one route that is like an api endpoint to get an specific transaction_category - '/transaction-categories/{type}'.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\TransactionCategory;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WalletController;
+use App\Http\Controllers\TransactionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,3 +29,48 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+Route::get('/transactions', [TransactionController::class, 'index'])
+    ->middleware('auth')
+    ->name('transactions.index');
+
+Route::get('/transactions/create', [TransactionController::class, 'create'])
+    ->middleware('auth')
+    ->name('transactions.create');
+
+Route::post('/transactions', [TransactionController::class, 'store'])
+    ->middleware('auth')
+    ->name('transactions.store');
+
+Route::get('/transactions/{transaction}', [
+    TransactionController::class,
+    'show',
+])
+    ->middleware('auth')
+    ->name('transactions.show');
+
+Route::get('transactions/{transaction}/edit', [
+    TransactionController::class,
+    'edit',
+])
+    ->middleware('auth')
+    ->name('transactions.edit');
+
+Route::put('/transactions/{transaction}', [
+    TransactionController::class,
+    'update',
+])
+    ->middleware('auth')
+    ->name('transactions.update');
+
+Route::delete('/transactions/{transaction}', [
+    TransactionController::class,
+    'destroy',
+])
+    ->middleware('auth')
+    ->name('transactions.destroy');
+
+Route::get('/transaction-categories/{type}', function ($type) {
+    $categories = TransactionCategory::where('transaction_type', $type)->get();
+    return response()->json($categories);
+});
