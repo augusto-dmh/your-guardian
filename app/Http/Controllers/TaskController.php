6809d5e7b5e16fc66778eb7a60f55020188e7863
= <augustodemeloh@protonmail.com>
2024-05-31 13:20:17 -0300

    feat: task controller
    task controller with all necessary methods to create a task (index, show, store, update,delete, create, edit) following the convention adopted.

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use App\Models\Task;
+use App\Models\TaskCategory;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Auth::user()->tasks()->get();
+
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function create()
+    {
+        $taskCategories = TaskCategory::all();
+
+        return view('tasks.create', compact('taskCategories'));
+    }
+
+    public function store(Request $request)
+    {
+        Auth::user()
+            ->tasks()
+            ->create([
+                'name' => $request->name,
+                'title' => $request->title,
+                'due_date' => $request->due_date,
+                'description' => $request->description,
+                'task_category_id' => TaskCategory::where([
+                    'name' => $request->category,
+                ])->first()->id,
+            ]);
+
+        return redirect()->back();
+    }
+
+    public function show(Task $task)
+    {
+        return view('tasks.show', compact('task'));
+    }
+
+    public function edit(Task $task)
+    {
+        return view('tasks.edit', compact('task'));
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all());
+
+        return redirect()->back();
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return redirect()->back();
+    }
+}
