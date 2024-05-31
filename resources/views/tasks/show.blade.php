7754056dd7cada7f61733ae6a33751cdd7e4c9e9
augusto-dmh <augustodemelohenriques@gmail.com>
2024-05-31 15:25:05 -0300

    feat: tasks show view
    tasks view for showing a particular task from the authenticated user.

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,22 @@
+<h2>Task</h2>
+<table>
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Due Date</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>{{ $task->title }}</td>
+            <td>{{ $task->description }}</td>
+            <td>{{ $task->taskCategory->name }}</td>
+            <td>{{ $task->due_date->format('m-d-Y') }}</td>
+            <td>{{ $task->status }}</td>
+        </tr>
+    </tbody>
+</table>
+
