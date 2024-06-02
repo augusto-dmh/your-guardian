86fa252d9e9e9a05ba592f4e243cfb9f7dd43491
= <augustodemeloh@protonmail.com>
2024-06-02 19:43:29 -0300

    feat: sendBillDueTomorrowNotifications schedulement
    Definition of schedulement of sendBIllDueTomorrowNotifications to be executed daily at midnight - related to BR/SP timezone.

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })
     ->purpose('Display an inspiring quote')
     ->hourly();
+
+Schedule::command('send:bill-due-tomorrow-notifications')
+    ->timezone('America/Sao_Paulo')
+    ->daily();
