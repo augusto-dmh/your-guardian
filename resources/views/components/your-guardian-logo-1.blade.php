de26b0ae392cb5cd83e226de317c2a84e0b4dc4b
= <augustodemeloh@protonmail.com>
2024-06-01 21:30:53 -0300

    feat: logo-v1 component
    component with the application's logo image (version 1; there are other 2).

---
+++ resources/views/components/your-guardian-logo-1.blade.php
@@ -0,0 +1,6 @@
+<div class="flex justify-center">
+    <a href="{{ route('dashboard') }}">
+        <img src="{{ asset('assets/logos/your-guardian-logo.svg') }}" alt="" class="logo-v1">
+    </a>
+</div>
+
