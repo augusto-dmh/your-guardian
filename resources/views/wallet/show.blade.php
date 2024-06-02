cba00e23749344fff7422ba1fda46525e20e6484
= <augustodemeloh@protonmail.com>
2024-06-02 00:16:50 -0300

    feat: wallet view style
    The view for showing the wallet is now styled based on css rules defined in app.css.

--- resources/views/wallet/show.blade.php
+++ resources/views/wallet/show.blade.php
@@ -4,21 +4,27 @@ use Illuminate\Support\Str;
 $user = auth()->user();
 ?>
 
-<div>
-    <h3>{{ $user->full_name }}</h2>
+<x-layout>
+    <div class="wallet">
+        <h3>{{ $user->full_name }}</h2>
 
-        <p>Balance: ${{ $user->balance }}</p>
-        <p>Next Bill Due: {{ $user->bills()->orderBy('due_date', 'asc')->first() ?? 'none' }}</p>
-        <p>Next Task Due: {{ $user->tasks()->orderBy('due_date', 'asc')->first() ?? 'none' }}</p>
-        <p>Last Transaction:
-            @if ($lastTransaction = $user->transactions->last())
-                Amount: ${{ $lastTransaction->amount }} |
-                Type: {{ $lastTransaction->type }} |
-                Category: {{ $lastTransaction->transactionCategory->name ?? 'none' }} |
-                Description: {{ Str::limit($lastTransaction->description, 5, '...') ?? 'none' }}
-            @else
-                None
-            @endif
-        </p>
-</div>
+            <p><span>Balance:</span> ${{ $user->balance }}</p>
+            <p><span>Next Bill Due:</span>
+                {{ $user->bills()->orderBy('due_date', 'asc')->first()->due_date->format('Y-m-d') ?? 'none' }}
+            </p>
+            <p><span>Next Task Due:</span>
+                {{ $user->tasks()->orderBy('due_date', 'asc')->first()->due_date->format('Y-m-d') ?? 'none' }}
+            </p>
+            <p><span>Last Transaction:</span>
+                @if ($lastTransaction = $user->transactions->last())
+                    Amount: ${{ $lastTransaction->amount }} |
+                    Type: {{ $lastTransaction->type }} |
+                    Category: {{ $lastTransaction->transactionCategory->name ?? 'none' }} |
+                    Description: {{ Str::limit($lastTransaction->description, 5, '...') ?? 'none' }}
+                @else
+                    None
+                @endif
+            </p>
+    </div>
+</x-layout>
 
