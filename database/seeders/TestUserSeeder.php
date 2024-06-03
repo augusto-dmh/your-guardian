fd3baf61468256e1d7dfea0c1d7250ad4794fd67
augusto-dmh <augustodemelohenriques@gmail.com>
2024-06-03 15:04:39 -0300

    feat: TestUserSeeder
    Seeder for creating a user for testing purposes every time the database must be refreshed - php artisan migrate:refresh --seed.

---
+++ database/seeders/TestUserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Bill;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class TestUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $user = User::create([
+            'first_name' => 'Augusto',
+            'last_name' => 'Henriques',
+            'birthdate' => '1990-06-03',
+            'email' => 'augustodemelohenriques@gmail.com',
+            'password' => '123123123',
+        ]);
+
+        Bill::factory()
+            ->count(10)
+            ->create([
+                'user_id' => $user->id,
+            ]);
+    }
+}
