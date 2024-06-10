e2b0ffec42b505c4ad36219967f509404bc16bef
augusto-dmh <augustodemelohenriques@gmail.com>
2024-06-10 16:53:11 -0300

    feat: create bill and task events
    the events to be dispatched after the entities instances are created/updated/deleted.

---
+++ app/Events/BillUpdated.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Bill;
+use App\Events\EventInterface;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class BillUpdated implements EventInterface
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Bill $bill)
+    {
+        $this->bill = $bill;
+    }
+
+    public function getEntity(): Bill
+    {
+        return $this->bill;
+    }
+}
