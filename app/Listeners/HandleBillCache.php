fcf74311ad3e50f9eca25ede01a4151e9171e6d8
augusto-dmh <augustodemelohenriques@gmail.com>
2024-06-10 17:13:51 -0300

    feat: create listeners for bill and task
    listener classes that listen to the entities events and execute the caching logic necessary after a bill or task is created/updated/deleted. These listeners replace the previous cacheHandlers.

---
+++ app/Listeners/HandleBillCache.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\BillCreated;
+use App\Events\BillDeleted;
+use App\Events\BillUpdated;
+use App\Events\EventInterface;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class HandleBillCache
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public static function handle(BillCreated|BillUpdated|BillDeleted $event)
+    {
+        $bill = $event->getEntity();
+
+        if ($bill->wasRecentlyCreated) {
+            self::handleCreatedBill($event);
+        } elseif ($bill->wasChanged()) {
+            self::handleUpdatedBill($event);
+        } elseif (!$bill->exists) {
+            self::handleDeletedBill($event);
+        }
+    }
+
+    private static function handleCreatedBill(BillCreated $event)
+    {
+        $bill = $event->getEntity();
+
+        if ($bill->due_date->isFuture() && $bill->status == 'pending') {
+            $nextPendingBillDueDate = Cache::get(
+                "user_{$bill->user_id}_next_bill_due"
+            );
+
+            if (
+                !$nextPendingBillDueDate ||
+                $bill->due_date < $nextPendingBillDueDate
+            ) {
+                Cache::put(
+                    "user_{$bill->user_id}_next_bill_due",
+                    $bill->due_date,
+                    60
+                );
+            }
+        }
+    }
+
+    private static function handleUpdatedBill(BillUpdated $event)
+    {
+        $bill = $event->getEntity();
+
+        if ($bill->due_date->isFuture() && $bill->status == 'pending') {
+            $nextPendingBillDueDate =
+                Cache::get("user_{$bill->user_id}_next_bill_due") ??
+                $bill->user
+                    ->bills()
+                    ->where('due_date', '>=', now())
+                    ->where('status', '=', 'pending')
+                    ->orderBy('due_date', 'asc')
+                    ->first()?->due_date;
+
+            $bill->due_date < $nextPendingBillDueDate
+                ? Cache::put(
+                    "user_{$bill->user_id}_next_bill_due",
+                    $bill->due_date,
+                    60
+                )
+                : Cache::add(
+                    "user_{$bill->user_id}_next_bill_due",
+                    $nextPendingBillDueDate,
+                    60
+                );
+        }
+    }
+
+    private static function handleDeletedBill(BillDeleted $event)
+    {
+        $bill = $event->getEntity();
+
+        if (
+            $bill->due_date->isFuture() &&
+            $bill->status == 'pending' &&
+            Cache::get("user_{$bill->user_id}_next_bill_due") == $bill->due_date
+        ) {
+            Cache::put(
+                "user_{$bill->user_id}_next_bill_due",
+                $bill->user
+                    ->bills()
+                    ->where('due_date', '>=', now())
+                    ->where('status', '=', 'pending')
+                    ->orderBy('due_date', 'asc')
+                    ->first()?->due_date,
+                60
+            );
+        }
+    }
+}
---
+++ app/Listeners/HandleTaskCache.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\TaskCreated;
+use App\Events\TaskDeleted;
+use App\Events\EventInterface;
+use App\Events\TaskUpdated;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class HandleTaskCache
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public static function handle(TaskCreated|TaskUpdated|TaskDeleted $event)
+    {
+        $task = $event->getEntity();
+        if ($task->wasRecentlyCreated) {
+            self::handleCreatedTask($event);
+        } elseif ($task->wasChanged()) {
+            self::handleUpdatedTask($event);
+        } elseif (!$task->exists) {
+            self::handleDeletedTask($event);
+        }
+    }
+
+    private static function handleCreatedTask(TaskCreated $event)
+    {
+        $task = $event->getEntity();
+
+        if ($task->due_date->isFuture() && $task->status == 'pending') {
+            $nextPendingTaskDueDate = Cache::get(
+                "user_{$task->user_id}_next_task_due"
+            );
+
+            if (
+                !$nextPendingTaskDueDate ||
+                $task->due_date < $nextPendingTaskDueDate
+            ) {
+                Cache::put(
+                    "user_{$task->user_id}_next_task_due",
+                    $task->due_date,
+                    60
+                );
+            }
+        }
+    }
+
+    private static function handleUpdatedTask(TaskUpdated $event)
+    {
+        $task = $event->getEntity();
+
+        if ($task->due_date->isFuture() && $task->status == 'pending') {
+            $nextPendingTaskDueDate =
+                Cache::get("user_{$task->user_id}_next_task_due") ??
+                $task->user
+                    ->tasks()
+                    ->where('due_date', '>=', now())
+                    ->where('status', '=', 'pending')
+                    ->orderBy('due_date', 'asc')
+                    ->first()?->due_date;
+
+            $task->due_date < $nextPendingTaskDueDate
+                ? Cache::put(
+                    "user_{$task->user_id}_next_task_due",
+                    $task->due_date,
+                    60
+                )
+                : Cache::add(
+                    "user_{$task->user_id}_next_task_due",
+                    $nextPendingTaskDueDate,
+                    60
+                );
+        }
+    }
+
+    private static function handleDeletedTask(TaskDeleted $event)
+    {
+        $task = $event->getEntity();
+
+        if (
+            $task->due_date->isFuture() &&
+            $task->status == 'pending' &&
+            Cache::get("user_{$task->user_id}_next_task_due") == $task->due_date
+        ) {
+            Cache::put(
+                "user_{$task->user_id}_next_task_due",
+                $task->user
+                    ->tasks()
+                    ->where('due_date', '>=', now())
+                    ->where('status', '=', 'pending')
+                    ->orderBy('due_date', 'asc')
+                    ->first()?->due_date,
+                60
+            );
+        }
+    }
+}
