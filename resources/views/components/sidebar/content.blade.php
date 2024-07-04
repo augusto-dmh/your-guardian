763d796763ba0810ad33dc54c4f49c73418cd4fb
= <augustodemeloh@protonmail.com>
2024-07-04 11:09:58 -0300

    style: update dropdowns icons and text
    icons for each dropdown have been updated: one main dropdown will be 'charts', which will have two children that will have options related to transactions and bills charts.

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -6,14 +6,14 @@
         </x-slot>
     </x-sidebar.link>
 
-    <x-sidebar.dropdown title="Buttons" :active="Str::startsWith(request()->route()->uri(), 'buttons')" isParentDropdown>
+    <x-sidebar.dropdown title="Charts" :active="Str::startsWith(request()->route()->uri(), 'buttons')" isParentDropdown>
         <x-slot name="icon">
-            <x-heroicon-o-view-grid class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+            <x-heroicon-o-chart-bar class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
 
-        <x-sidebar.dropdown title="Buttons" :active="Str::startsWith(request()->route()->uri(), 'buttons')">
+        <x-sidebar.dropdown title="Transactions" :active="Str::startsWith(request()->route()->uri(), 'buttons')">
             <x-slot name="icon">
-                <x-heroicon-o-view-grid class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+                <x-heroicon-s-switch-horizontal class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
             </x-slot>
 
             <x-sidebar.sublink title="Text button" href="{{ route('buttons.text') }}" :active="request()->routeIs('buttons.text')" />
@@ -21,9 +21,9 @@
             <x-sidebar.sublink title="Text with icon" href="{{ route('buttons.text-icon') }}" :active="request()->routeIs('buttons.text-icon')" />
         </x-sidebar.dropdown>
 
-        <x-sidebar.dropdown title="Buttons" :active="Str::startsWith(request()->route()->uri(), 'buttons')">
+        <x-sidebar.dropdown title="Bills" :active="Str::startsWith(request()->route()->uri(), 'buttons')">
             <x-slot name="icon">
-                <x-heroicon-o-view-grid class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+                <x-heroicon-o-document-text class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
             </x-slot>
 
             <x-sidebar.sublink title="Text button" href="{{ route('buttons.text') }}" :active="request()->routeIs('buttons.text')" />
