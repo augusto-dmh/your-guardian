322ca9f67dadca1bdc5df92bb3d6071f98fd048e
= <augustodemeloh@protonmail.com>
2024-07-04 11:06:50 -0300

    feat: add parentDropdown logic to change inner style
    added logic for, depending on whether the dropdown is the parent of others or not, change the inner styling related to the gray lines that connects sublinks to dropdowns (only child dropdowns will have them).

--- resources/views/components/sidebar/dropdown.blade.php
+++ resources/views/components/sidebar/dropdown.blade.php
@@ -1,6 +1,7 @@
 @props([
     'active' => false,
-    'title' => ''
+    'title' => '',
+    'isParentDropdown' => false
 ])
 
 <div class="relative" x-data="{ open: @json($active) }">
@@ -14,8 +15,9 @@
 
     <div x-show="open && (isSidebarOpen || isSidebarHovered)" x-collapse>
         <ul
-            class="relative px-0 pt-2 pb-0 ml-5 before:w-0 before:block before:absolute before:inset-y-0 before:left-0 before:border-l-2 before:border-l-gray-200 dark:before:border-l-gray-600">
+            class="relative px-0 pt-2 pb-0 ml-5 @if (!$isParentDropdown) before:w-0 before:block before:absolute before:inset-y-0 before:left-0 before:border-l-2 before:border-l-gray-200 dark:before:border-l-gray-600 @endif">
             {{ $slot }}
         </ul>
     </div>
 </div>
+
