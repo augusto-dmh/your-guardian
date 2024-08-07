50cf4c8aad2a5c901b3c2cd7bf889acbd016bcaf
= <augustodemeloh@protonmail.com>
2024-08-07 10:22:24 -0300

    feat: add tableIndexRow component
    calculates and shows dynamically the fillable attribute values of a particular model instance in the rows of the index views tables.

---
+++ app/View/Components/tableIndexRow.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use App\Models\Bill;
+use App\Models\Task;
+use App\Models\Transaction;
+use InvalidArgumentException;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class tableIndexRow extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public string $entityName,
+        public $entityInstance
+    ) {
+        if (
+            !(
+                $entityInstance instanceof Transaction ||
+                $entityInstance instanceof Bill ||
+                $entityInstance instanceof Task
+            )
+        ) {
+            throw new InvalidArgumentException(
+                'Entity instance must be an instance of Transaction, Bill, or Task.'
+            );
+        }
+
+        $this->entityInstance = $entityInstance;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.table-index-row');
+    }
+}
