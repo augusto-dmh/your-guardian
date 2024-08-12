c747b23d5d680f8ca9c9d853dcd885f9fa29a96f
= <augustodemeloh@protonmail.com>
2024-08-11 23:45:30 -0300

    feat: disable triggers + simulate 'insert' ones in TestUserSeeder
    There are insert triggers that, when executed on test data, create inconsistencies (e.g: when a paid bill is inserted, a transaction is created based on it and the 'created_at' field receives the current timestamp, but considering that the bill that originate the transaction can have a random 'paid_at'/'due_date', that is ilogic). So that's why the triggers are disabled and the behavior of insert ones are simulated instead, avoiding these inconsistencies.

--- database/seeders/TestUserSeeder.php
+++ database/seeders/TestUserSeeder.php
@@ -4,9 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\Bill;
 use App\Models\Task;
-use App\Models\Transaction;
 use App\Models\User;
+use App\Models\Transaction;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class TestUserSeeder extends Seeder
@@ -16,6 +17,8 @@ class TestUserSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET @DISABLE_TRIGGERS = TRUE;');
+
         $user = User::create([
             'first_name' => 'Augusto',
             'last_name' => 'Henriques',
@@ -25,21 +28,53 @@ class TestUserSeeder extends Seeder
         ]);
 
         Bill::factory()
-            ->count(10)
+            ->count(5000)
             ->create([
                 'user_id' => $user->id,
             ]);
+
+        // bills trigger to create transaction based on paid bill
+        $bills = $user->bills;
+        foreach ($bills as $bill) {
+            if ($bill->status === 'paid') {
+                if (mt_rand(0, 1)) {
+                    $bill->paid_at = fake()->date($bill->due_date);
+                    $bill->save();
+                }
+
+                $bill->transactions()->create([
+                    'user_id' => $bill->user_id,
+                    'bill_id' => $bill->id,
+                    'amount' => $bill->amount,
+                    'type' => 'expense',
+                    'title' => $bill->title,
+                    'description' => $bill->description,
+                ]);
+            }
+        }
 
         Transaction::factory()
-            ->count(10)
+            ->count(5000)
             ->create([
                 'user_id' => $user->id,
             ]);
 
+        // transactions trigger to ensure amount is consistent with type
+        $transactions = $user->transactions;
+        foreach ($transactions as $transaction) {
+            $transaction->amount =
+                $transaction->type === 'income'
+                    ? abs($transaction->amount)
+                    : -abs($transaction->amount);
+        }
+        $transactions->each->save();
+
         Task::factory()
-            ->count(10)
+            ->count(5000)
             ->create([
                 'user_id' => $user->id,
             ]);
+
+        DB::statement('SET @DISABLE_TRIGGERS = FALSE;');
     }
 }
