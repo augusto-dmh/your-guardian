430cfb0dad3f5eead01de7898e34ecb9fe7e56e8
= <augustodemeloh@protonmail.com>
2024-08-19 01:49:23 -0300

    feat: add getDateFormatForChartDataQuery helper
    it returns the dateFormat to be applied in the chart-related queries customized based on the user prefered language.

--- app/helpers.php
+++ app/helpers.php
@@ -15,3 +15,15 @@ if (!function_exists('formatDate')) {
         return Carbon::parse($date)->format($format);
     }
 }
+
+if (!function_exists('getDateFormatForChartDataQuery')) {
+    function getDateFormatForChartDataQuery($format = 'Y-m-d')
+    {
+        $locale = app()->getLocale();
+
+        if ($format === 'Y-m') {
+            return $locale === 'pt_BR' ? '%m-%Y' : '%Y-%m';
+        }
+        return $locale === 'pt_BR' ? '%d-%m-%Y' : '%Y-%m-%d';
+    }
+}
