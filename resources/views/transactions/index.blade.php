45e79b17d3e63241dcf23160994e9cdb97ee8a63
= <augustodemeloh@protonmail.com>
2024-08-21 00:40:25 -0300

    feat: add search input to index views

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -7,7 +7,7 @@
     </x-slot>
 
     <form method="GET" action="{{ route('transactions.index') }}">
-        <div class="flex flex-col gap-3 pb-8 m-auto mt-6 lg:justify-between xl:justify-start lg:flex-row xl:gap-12">
+        <div class="flex flex-col gap-3 pb-4 m-auto lg:justify-between xl:justify-start lg:flex-row xl:gap-12">
             <div class="flex items-end gap-4 md:flex-row">
                 <div class="flex items-end gap-4">
                     <div class="flex items-end gap-2">
@@ -81,6 +81,24 @@
                         class="text-sm font-medium text-gray-900 ms-3 dark:text-gray-300">{{ __('Table view') }}</span>
                 </label>
             </div>
+        </div>
+    </form>
+
+    <form class="max-w-lg pb-8">
+        <label for="default-search" class="mb-2 font-medium text-gray-900 sr-only">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+            </div>
+            <input type="search" name="searchTerm" value="{{ $searchTerm }}"
+                class="block w-full p-4 text-white bg-opacity-50 border-none rounded-lg appearance-none bg-secondary-bg ps-10 focus:outline-none focus:ring-2 focus:ring-quinary-bg"
+                placeholder="{{ __('Search for title or description') }}" />
+            <button type="submit"
+                class="right-0 absolute end-2.5 bottom-2.5 bg-primary-bg shadow-inner hover:shadow-innerHover text-tertiary-txt font-medium rounded-lg text-sm px-4 py-2">Search</button>
         </div>
     </form>
 
