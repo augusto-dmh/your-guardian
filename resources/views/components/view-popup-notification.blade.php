95a4e30a288d11886a242c40af3787458fd1603f
= <augustodemeloh@protonmail.com>
2024-09-25 23:46:15 -0300

    feat: add close button to the popup container

--- resources/views/components/view-popup-notification.blade.php
+++ resources/views/components/view-popup-notification.blade.php
@@ -1,4 +1,4 @@
-<div class="flex justify-center">
+<div class="flex justify-center popup-container">
     <div class="fixed z-10 px-4 py-2 text-2xl rounded-md shadow-inner top-6 text-tertiary-txt bg-primary-bg"
         role="alert">
         <div class="flex flex-col items-center">
@@ -9,5 +9,28 @@
                 {{ __($ctaText) }}!
             </a>
         </div>
+        <button type="button" class="absolute appearance-none cursor-pointer popup-close-btn right-2 top-14">
+            <x-heroicon-o-x class="w-6 h-6" />
+        </button>
     </div>
 </div>
+
+<style>
+    .popup-container {
+        transition: opacity 0.25s ease-in-out;
+    }
+
+    .hide {
+        opacity: 0;
+    }
+</style>
+
+<script>
+    const popupContainer = document.querySelector('.popup-container');
+    const popupCloseBtn = document.querySelector('.popup-close-btn');
+
+    popupCloseBtn.addEventListener('click', () => {
+        popupContainer.classList.add('hide');
+        setTimeout(() => popupContainer.remove(), 250);
+    })
+</script>
