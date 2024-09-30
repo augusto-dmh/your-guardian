cc3fbf26dfc50eab974f9af3a5fbc814e9bb6795
= <augustodemeloh@protonmail.com>
2024-09-29 21:59:59 -0300

    refactor: modify structure and style of edit views

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,68 +1,84 @@
-<x-layout>
-    <div class="form-wrapper">
-        <div class="flex justify-center">
+<x-app-layout>
+    <x-slot name="header"></x-slot>
+
+    <div class="p-6 m-auto rounded-sm shadow-inner form-wrapper h-fit w-fit">
+        <div class="w-20 h-20 m-auto">
             <a href="{{ route('dashboard') }}">
-                <x-application-logo class="w-32 h-32" />
+                <x-application-logo class="w-full h-full" />
             </a>
         </div>
 
-        <form action="{{ route('tasks.update', $task) }}" method="post" class="form-main">
+        <form action="{{ route('tasks.update', $task) }}" method="post">
             @csrf
             @method('PUT')
 
-            <fieldset>
-                <div class="form-group">
-                    <label for="title">Title:</label>
-                    <input type="text" name="title" placeholder="Title" value="{{ old('title', $task->title) }}">
+            <fieldset class="flex flex-col gap-4">
+                <div class="flex flex-col gap-1">
+                    <label for="title" class="cursor-pointer text-secondary-txt">{{ __('Title') }}:</label>
+                    <input type="text" name="title" placeholder="Title" value="{{ old('title', $task->title) }}"
+                        id="title"
+                        class="block w-full text-gray-300 bg-opacity-50 border-none rounded-lg appearance-none bg-tertiary-bg ps-4 pe-[4.75rem] focus:outline-none focus:ring-2 focus:ring-quinary-bg">
                     @error('title')
-                        <p>{{ $message }}</p>
+                        <p class="text-red-500">{{ $message }}</p>
                     @enderror
                 </div>
-                <div class="form-group">
-                    <label for="due_date">Due date:</label>
-                    <input type="date" name="due_date" id="due_date"
-                        value="{{ old('due_date', formatDate($task->due_date)) }}">
-                    @error('due_date')
-                        <p>{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="task_category_id">Category:</label>
-                    <select name="task_category_id" id="task_category_id">
-                        <option value="">Select a category</option>
 
+                <div class="flex flex-col gap-1">
+                    <label for="due_date" class="cursor-pointer text-secondary-txt">{{ __('Due Date') }}:</label>
+                    <input type="date" name="due_date" id="due_date"
+                        value="{{ old('due_date', formatDate($task->due_date)) }}"
+                        class="block w-full text-gray-300 bg-opacity-50 border-none rounded-lg appearance-none bg-tertiary-bg ps-4 focus:outline-none focus:ring-2 focus:ring-quinary-bg">
+                    @error('due_date')
+                        <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <label for="task_category_id"
+                        class="cursor-pointer text-secondary-txt">{{ __('Category') }}:</label>
+                    <select name="task_category_id" id="task_category_id"
+                        class="font-thin text-gray-300 border-0 cursor-pointer focus:outline-none focus:ring-2 focus:ring-quinary-bg bg-secondary-bg hover:bg-tertiary-bg focus:bg-tertiary-bg">
+                        <option value="">{{ __('Select a Category') }}</option>
                         @foreach ($taskCategories as $taskCategory)
                             <option value="{{ $taskCategory->id }}"
                                 {{ old('task_category_id', $task->taskCategory->id) === $taskCategory->id ? 'selected' : '' }}>
-                                {{ ucFirst($taskCategory->name) }}</option>
+                                {{ ucfirst($taskCategory->name) }}</option>
                         @endforeach
                     </select>
                 </div>
-                <div class="form-group">
-                    <label for="status">Status:</label>
-                    <select name="status" id="status">
+
+                <div class="flex flex-col gap-1">
+                    <label for="status" class="cursor-pointer text-secondary-txt">{{ __('Status') }}:</label>
+                    <select name="status" id="status"
+                        class="font-thin text-gray-300 border-0 cursor-pointer focus:outline-none focus:ring-2 focus:ring-quinary-bg bg-secondary-bg hover:bg-tertiary-bg focus:bg-tertiary-bg">
                         <option value="pending" {{ old('status', $task->status) === 'pending' ? 'selected' : '' }}>
                             Pending
                         </option>
                         <option value="completed" {{ old('status', $task->status) === 'completed' ? 'selected' : '' }}>
                             Completed
                         </option>
-                        <option value="failed" {{ old('status', $task->status) === 'failed' ? 'selected' : '' }}>Failed
+                        <option value="failed" {{ old('status', $task->status) === 'failed' ? 'selected' : '' }}>
+                            Failed
                         </option>
                     </select>
                 </div>
-                <div class="form-group">
-                    <label for="description">Description:</label>
-                    <textarea id="description" name="description" rows="4" cols="50">{{ old('description', $task->description) }}</textarea>
+
+                <div class="flex flex-col gap-1">
+                    <label for="description" class="cursor-pointer text-secondary-txt">{{ __('Description') }}:</label>
+                    <textarea id="description" name="description" rows="4" cols="50"
+                        class="block w-full text-gray-300 bg-opacity-50 border-none rounded-lg appearance-none bg-tertiary-bg ps-4 pe-[4.75rem] focus:outline-none focus:ring-2 focus:ring-quinary-bg">{{ old('description', $task->description) }}</textarea>
                     @error('description')
-                        <p>{{ $message }}</p>
+                        <p class="text-red-500">{{ $message }}</p>
                     @enderror
                 </div>
             </fieldset>
 
-            <div class="form-group">
-                <button>Edit Task</button>
+            <div class="flex gap-3 mt-8">
+                <button type="submit"
+                    class="right-3 text-center w-full bottom-2.5 bg-primary-bg shadow-inner hover:shadow-innerHover text-tertiary-txt font-medium rounded-lg text-sm px-4 py-2">{{ __('Update') }}</button>
+                <a href="{{ route('bills.index') }}"
+                    class="right-3 text-center w-full bottom-2.5 bg-primary-bg shadow-inner hover:shadow-innerHover text-tertiary-txt font-medium rounded-lg text-sm px-4 py-2">{{ __('Back') }}</a>
             </div>
         </form>
     </div>
-</x-layout>
+</x-app-layout>
--- resources/views/transactions/edit.blade.php
+++ resources/views/transactions/edit.blade.php
@@ -1,39 +1,46 @@
-<x-layout>
-    <div id="overlay" class="fixed inset-0 z-10 hidden cursor-pointer"></div>
+<x-app-layout>
+    <x-slot name="header"></x-slot>
 
-    <h2 class="absolute top-0 hidden transform -translate-x-1/2 left-1/2" id="loading">Loading...</h2>
+    <div class="p-6 m-auto rounded-sm shadow-inner form-wrapper h-fit w-fit">
+        <h2 class="absolute z-10 hidden text-xl transform -translate-x-1/2 top-3" id="loading"
+            style="left: calc(50% + 1.5rem);">
+            Loading...</h2>
 
-    <div class="form-wrapper">
-        <div class="flex justify-center">
+        <div class="w-20 h-20 m-auto">
             <a href="{{ route('dashboard') }}">
-                <x-application-logo class="w-32 h-32" />
+                <x-application-logo class="w-full h-full" />
             </a>
         </div>
 
-        <form action="{{ route('transactions.update', $transaction) }}" method="post" class="form-main">
+        <form action="{{ route('transactions.update', $transaction) }}" method="post">
             @csrf
             @method('PUT')
 
-            <fieldset>
-                <div class="form-group">
-                    <label for="title">Title:</label>
+            <fieldset class="flex flex-col gap-4">
+                <div class="flex flex-col gap-1">
+                    <label for="title" class="cursor-pointer text-secondary-txt">{{ __('Title') }}:</label>
                     <input type="text" name="title" placeholder="Title"
-                        value="{{ old('title', $transaction->title) }}">
+                        value="{{ old('title', $transaction->title) }}" id="title"
+                        class="block w-full text-gray-300 bg-opacity-50 border-none rounded-lg appearance-none bg-tertiary-bg ps-4 pe-[4.75rem] focus:outline-none focus:ring-2 focus:ring-quinary-bg">
                     @error('title')
-                        <p>{{ $message }}</p>
+                        <p class="text-red-500">{{ $message }}</p>
                     @enderror
                 </div>
-                <div class="form-group">
-                    <label for="amount">Amount:</label>
+
+                <div class="flex flex-col gap-1">
+                    <label for="amount" class="cursor-pointer text-secondary-txt">{{ __('Amount') }}:</label>
                     <input type="text" name="amount" placeholder="Amount"
-                        value="{{ old('amount', $transaction->amount) }}">
+                        value="{{ old('amount', $transaction->amount) }}" id="amount"
+                        class="block w-full text-gray-300 bg-opacity-50 border-none rounded-lg appearance-none bg-tertiary-bg ps-4 pe-[4.75rem] focus:outline-none focus:ring-2 focus:ring-quinary-bg">
                     @error('amount')
-                        <p>{{ $message }}</p>
+                        <p class="text-red-500">{{ $message }}</p>
                     @enderror
                 </div>
-                <div class="form-group">
-                    <label for="type">Type:</label>
-                    <select id="type" name="type">
+
+                <div class="flex flex-col gap-1">
+                    <label for="type" class="cursor-pointer text-secondary-txt">{{ __('Type') }}:</label>
+                    <select id="type" name="type"
+                        class="font-thin text-gray-300 border-0 cursor-pointer focus:outline-none focus:ring-2 focus:ring-quinary-bg bg-secondary-bg hover:bg-tertiary-bg focus:bg-tertiary-bg">
                         <option value="income" {{ old('type', $transaction->type) === 'income' ? 'selected' : '' }}>
                             Income
                         </option>
@@ -42,59 +49,64 @@
                         </option>
                     </select>
                 </div>
-                <div class="form-group">
-                    <label for="transaction_category_id">Category:</label>
-                    <select name="transaction_category_id" id="transaction_category_id">
+
+                <div class="flex flex-col gap-1">
+                    <label for="transaction_category_id"
+                        class="cursor-pointer text-secondary-txt">{{ __('Category') }}:</label>
+                    <select name="transaction_category_id" id="transaction_category_id"
+                        class="font-thin text-gray-300 border-0 cursor-pointer focus:outline-none focus:ring-2 focus:ring-quinary-bg bg-secondary-bg hover:bg-tertiary-bg focus:bg-tertiary-bg">
                         @foreach ($transactionCategories as $transactionCategory)
                             <option value="{{ $transactionCategory->id }}"
-                                {{ old('transaction_category_id', $transaction->transactionCategory->id) == $transactionCategory->id ? 'selected' : '' }}>
+                                {{ old('transaction_category_id', $transaction->transactionCategory?->id) == $transactionCategory->id ? 'selected' : '' }}>
                                 {{ $transactionCategory->name }}
                             </option>
                         @endforeach
                     </select>
                 </div>
-                <div class="form-group">
-                    <label for="description">Description:</label>
-                    <textarea id="description" name="description" rows="4" cols="50">{{ old('description', $transaction->description) }}</textarea>
+
+                <div class="flex flex-col gap-1">
+                    <label for="description" class="cursor-pointer text-secondary-txt">{{ __('Description') }}:</label>
+                    <textarea id="description" name="description" rows="4" cols="50"
+                        class="block w-full text-gray-300 bg-opacity-50 border-none rounded-lg appearance-none bg-tertiary-bg ps-4 pe-[4.75rem] focus:outline-none focus:ring-2 focus:ring-quinary-bg">{{ old('description', $transaction->description) }}</textarea>
                     @error('description')
-                        <p>{{ $message }}</p>
+                        <p class="text-red-500">{{ $message }}</p>
                     @enderror
                 </div>
             </fieldset>
 
-            <div class="form-group">
-                <button>Update</button>
+            <div class="flex gap-3 mt-8">
+                <button type="submit"
+                    class="right-3 text-center w-full bottom-2.5 bg-primary-bg shadow-inner hover:shadow-innerHover text-tertiary-txt font-medium rounded-lg text-sm px-4 py-2">{{ __('Update') }}</button>
+                <a href="{{ route('transactions.index') }}"
+                    class="right-3 text-center w-full bottom-2.5 bg-primary-bg shadow-inner hover:shadow-innerHover text-tertiary-txt font-medium rounded-lg text-sm px-4 py-2">{{ __('Back') }}</a>
             </div>
         </form>
-
-        <script defer>
-            let isFirstRender = true;
-
-            document.getElementById('type').addEventListener('change', async function() {
-                if (isFirstRender) {
-                    isFirstRender = false;
-                    return;
-                }
-                const loadingElement = document.getElementById('loading');
-                const overlayElement = document.getElementById('overlay');
-                loadingElement.classList.remove('hidden');
-                overlayElement.classList.remove('hidden');
-
-                const type = this.value;
-                const response = await fetch('/transaction-categories/' + type);
-                const categories = await response.json();
-                const categorySelect = document.getElementById('transaction_category_id');
-                const oldCategoryId = "{{ old('transaction_category_id') }}";
-
-                categorySelect.innerHTML = categories.map(function(category) {
-                    return `<option value="${category.id}" ${oldCategoryId == category.id ? 'selected' : ''}>${category.name}</option>`;
-                }).join('');
-
-                loadingElement.classList.add('hidden');
-                overlayElement.classList.add('hidden');
-            });
-
-            document.getElementById('type').dispatchEvent(new Event('change'));
-        </script>
     </div>
-</x-layout>
+</x-app-layout>
+
+<script defer>
+    let isFirstRender = true;
+
+    document.getElementById('type').addEventListener('change', async function() {
+        if (isFirstRender) {
+            isFirstRender = false;
+            return;
+        }
+        const loadingElement = document.getElementById('loading');
+        loadingElement.classList.remove('hidden');
+
+        const type = this.value;
+        const response = await fetch('/transaction-categories/' + type);
+        const categories = await response.json();
+        const categorySelect = document.getElementById('transaction_category_id');
+        const oldCategoryId = "{{ old('transaction_category_id') }}";
+
+        categorySelect.innerHTML = categories.map(function(category) {
+            return `<option value="${category.id}" ${oldCategoryId == category.id ? 'selected' : ''}>${category.name}</option>`;
+        }).join('');
+
+        loadingElement.classList.add('hidden');
+    });
+
+    document.getElementById('type').dispatchEvent(new Event('change'));
+</script>
