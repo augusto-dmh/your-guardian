a8023373be6e789c766bc7a68f516c3cea5bb166
= <augustodemeloh@protonmail.com>
2024-10-26 19:58:02 -0300

    feat: add BillCreateTest

--- tests/Feature/Bill/BillCreateTest.php
+++ tests/Feature/Bill/BillCreateTest.php
@@ -1,22 +1,73 @@
 <?php
 
+use App\Models\Bill;
 use App\Models\User;
-use Faker\Factory as Faker;
 
+use function Pest\Laravel\get;
+use function Pest\Laravel\actingAs;
+use function PHPUnit\Framework\assertStringContainsString;
 
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-beforeEach(function () {
-    $this->faker = Faker::create();
+test('can create view be showed to an authenticated user', function () {
+    $user = User::factory()->create();
+
+    $response = actingAs($user)->get(route('bills.create'));
+
+    $response
+        ->assertStatus(200)
+        ->assertViewIs('bills.create');
 });
 
-test('bill create view successfully showed', function () {
+
+test('cant create view be showed to a guest', function () {
+    $response = get(route('bills.create'));
+
+    $response
+        ->assertRedirectToRoute('login');
+});
+
+test('are fillable not-fk fields displayed in create view', function () {
     $user = User::factory()->create();
-    $this->actingAs($user);
+    $fillableFields = (new Bill)->getFillable();
+    $nonForeignKeyFields = array_filter($fillableFields, function ($field) {
+        return !str_ends_with($field, '_id');
+    });
 
-    $response = $this->get(route('bills.create'));
+    $response = actingAs($user)->get(route('bills.create'));
 
-    $response->assertStatus(200);
-    $response->assertViewIs('bills.create');
-    $response->assertSee('form');
+    $response
+        ->assertStatus(200);
+    foreach ($nonForeignKeyFields as $field) {
+        $labelText = ucwords(str_replace('_', ' ', $field));
+        $response
+            ->assertSeeText($labelText);
+    }
+});
+
+test('does the test has a csrf token', function () {
+    $user = User::factory()->create();
+
+    $response = actingAs($user)->get(route('bills.create'));
+
+    $response
+        ->assertSee('<input type="hidden" name="_token"', false);
+});
+
+test('does the form action is correct', function () {
+    $user = User::factory()->create();
+
+    $response = actingAs($user)->get(route('bills.create'));
+
+    $response
+        ->assertSee('<form action="' . route('bills.store') . '"', false);
+});
+
+test('does the form method is correct', function () {
+    $user = User::factory()->create();
+
+    $response = actingAs($user)->get(route('bills.create'));
+
+    $responseContent = strtolower($response->getContent());
+    assertStringContainsString('<form action="' . route('bills.store') . '" method="post"', $responseContent);
 });
