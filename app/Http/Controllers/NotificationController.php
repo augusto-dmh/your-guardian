cb693e7b97797fa71838703db1537adc3639dbbd
= <augustodemeloh@protonmail.com>
2024-11-18 00:03:34 -0300

    feat: add 'read' method to NotificationController

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Notifications\DatabaseNotification;
 
 class NotificationController extends Controller
 {
-    //
+    public function read(DatabaseNotification $notification)
+    {
+        $notification->markAsRead();
+
+        return redirect($notification->data['url']);
+    }
 }
