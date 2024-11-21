28bee83f8832518199d29879a436885f6755b17e
= <augustodemeloh@protonmail.com>
2024-11-21 11:48:40 -0300

    feat: add index and savePreferences methods to UserAvailableNotificationController
    the form data needs to be sent to only one endpoint, so there needs to be a method that enables both bulkStore and bulkDestroy of notifications enabled by the user: savePreferences. The 'store' and 'destroy' were used because i thought previously on having eventListeners attached to the checkboxes with these methods involved, but this approach would trigger one request per click, which is worse to performance than just one request-response cycle that deals with all the notifications storing/deletion.

--- app/Http/Controllers/UserAvailableNotificationController.php
+++ app/Http/Controllers/UserAvailableNotificationController.php
@@ -4,26 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Models\AvailableNotification;
 
 class UserAvailableNotificationController extends Controller
 {
-    public function store(Request $request)
+    public function index()
     {
-        DB::table('user_available_notifications')
-            ->updateOrInsert([
-                'user_id' => $request->user_id,
-                'available_notification_id' => $request->available_notification_id
-            ]);
+        $availableNotifications = AvailableNotification::all();
+        $userEnabledNotifications = auth()->user()->enabledNotifications;
+        Log::info($availableNotifications);
+        Log::info($userEnabledNotifications);
 
-        return redirect()->back()->with('success', __('Notifications preferences updated successfully'));
+        return view('user-available-notifications.index', compact('availableNotifications', 'userEnabledNotifications'));
     }
 
-    public function destroy(Request $request)
+    private function bulkStore($notificationsIds, $userId)
+    {
+        foreach ($notificationsIds as $notificationId) {
+            DB::table('user_available_notifications')
+                ->updateOrInsert([
+                    'user_id' => $userId,
+                    'available_notification_id' => $notificationId
+                ]);
+        }
+    }
+
+    private function bulkDestroy($notificationsIds, $userId)
     {
         DB::table('user_available_notifications')
-            ->where('user_id', $request->user_id)
-            ->where('available_notification_id', $request->available_notification_id)
+            ->where('user_id', $userId)
+            ->whereIn('available_notification_id', $notificationsIds)
             ->delete();
+    }
+
+    public function savePreferences(Request $request)
+    {
+        $userId = auth()->user()->id;
+        $availableNotifications = AvailableNotification::query()->pluck('id')->toArray();
+        $notificationsToEnabledIds = $request->input('notifications', []);
+
+        $notificationsToDisable = array_diff($availableNotifications, $notificationsToEnabledIds);
+
+        $notificationsToEnabledIds && $this->bulkStore($notificationsToEnabledIds, $userId);
+        $notificationsToDisable && $this->bulkDestroy($notificationsToDisable, $userId);
 
         return redirect()->back()->with('success', __('Notifications preferences updated successfully'));
     }
