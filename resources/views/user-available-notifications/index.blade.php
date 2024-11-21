8d9707d7958beb56aa4baf20f42668fd5474dea4
= <augustodemeloh@protonmail.com>
2024-11-21 11:54:27 -0300

    feat: add user-available-notifications/index view

---
+++ resources/views/user-available-notifications/index.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <div class="flex flex-col items-center justify-center gap-4">
+        <div>
+            <x-slot name="header">
+                <h2 class="text-4xl font-bold text-center text-secondary-txt">{{ __('Manage Notifications') }}</h2>
+            </x-slot>
+        </div>
+        <form action="{{ route('user-available-notifications.savePreferences') }}" method="POST" class="flex flex-col gap-2">
+            @csrf
+            <div class="flex flex-col gap-2">
+                @foreach ($availableNotifications as $notification)
+                    <div class="rounded-lg bg-secondary-bg p-2 {{ in_array($notification->id, $userEnabledNotifications->pluck('id')->toArray()) }}">
+                        <label class="flex items-center gap-2 cursor-pointer">
+                            <input type="checkbox" name="notifications[]" value="{{ $notification->id }}" {{ in_array($notification->id, $userEnabledNotifications->pluck('id')->toArray()) ? 'checked' : '' }} class="cursor-pointer border-1 focus:outline-none focus:ring-2 focus:ring-quinary-bg text-tertiary-txt bg-secondary-bg hover:bg-tertiary-bg focus:bg-tertiary-bg">
+                            <div class="flex flex-col select-none">
+                                <span>{{ $notification->name }}</span>
+                                <p class="italic text-gray-500"> {{ $notification->description }} </p>
+                            </div>
+                        </label>
+                    </div>
+                @endforeach
+            </div>
+            <div class="mt-4">
+                <button type="submit" class="px-4 py-1 shadow-inner text-tertiary-txt hover:shadow-innerHover hover:text-secondary-txt">Save Preferences</button>
+            </div>
+        </form>
+    </div>
+</x-app-layout>
