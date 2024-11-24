9c62c02f3428ee7effafe21f27ce688d848e0647
= <augustodemeloh@protonmail.com>
2024-11-24 17:20:48 -0300

    refactor: modify savePreferences from UserAvailableNotificationController to use 'sync' instead of 'bulk' methods

--- app/Http/Controllers/UserAvailableNotificationController.php
+++ app/Http/Controllers/UserAvailableNotificationController.php
@@ -13,41 +13,16 @@ class UserAvailableNotificationController extends Controller
     {
         $availableNotifications = AvailableNotification::all();
         $userEnabledNotifications = auth()->user()->enabledNotifications;
-        Log::info($availableNotifications);
-        Log::info($userEnabledNotifications);
 
         return view('user-available-notifications.index', compact('availableNotifications', 'userEnabledNotifications'));
     }
 
-    private function bulkStore($notificationsIds, $userId)
-    {
-        foreach ($notificationsIds as $notificationId) {
-            DB::table('available_notification_user')
-                ->updateOrInsert([
-                    'user_id' => $userId,
-                    'available_notification_id' => $notificationId
-                ]);
-        }
-    }
-
-    private function bulkDestroy($notificationsIds, $userId)
-    {
-        DB::table('available_notification_user')
-            ->where('user_id', $userId)
-            ->whereIn('available_notification_id', $notificationsIds)
-            ->delete();
-    }
-
     public function savePreferences(Request $request)
     {
-        $userId = auth()->user()->id;
-        $availableNotifications = AvailableNotification::query()->pluck('id')->toArray();
-        $notificationsToEnabledIds = $request->input('notifications', []);
+        $user = auth()->user();
+        $notificationsToBeEnabled = $request->input('notifications', []);
 
-        $notificationsToDisable = array_diff($availableNotifications, $notificationsToEnabledIds);
-
-        $notificationsToEnabledIds && $this->bulkStore($notificationsToEnabledIds, $userId);
-        $notificationsToDisable && $this->bulkDestroy($notificationsToDisable, $userId);
+        $user->enabledNotifications()->sync($notificationsToBeEnabled);
 
         return redirect()->back()->with('success', __('Notifications preferences updated successfully'));
     }
