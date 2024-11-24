34dd139c67c705af6ccf12d680c3add330f9748b
= <augustodemeloh@protonmail.com>
2024-11-24 17:05:29 -0300

    feat: add notifications-related tests

---
+++ tests/Feature/Notification/SendNotificationsBillsDueTomorrowTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Carbon\Carbon;
+use App\Models\Bill;
+use App\Models\User;
+use function Pest\Laravel\seed;
+use function Pest\Laravel\artisan;
+use App\Models\AvailableNotification;
+use Database\Seeders\AvailableNotificationsSeeder;
+
+test('can users with "Bills Due Tomorrow" notification enabled receive it in-app', function () {
+    seed(AvailableNotificationsSeeder::class);
+    $billsDueTomorrowNotificationId = AvailableNotification::where('name', 'Bills Due Tomorrow')->value('id');
+    $user = User::factory()->create();
+    $bill = Bill::factory()->create(['status' => 'pending', 'due_date' => Carbon::tomorrow()->format('Y-m-d'), 'user_id' => $user->id]);
+    $user->enabledNotifications()->attach($billsDueTomorrowNotificationId);
+
+    artisan('send-emails:bills-due-tomorrow');
+    artisan('queue:work', ['--stop-when-empty' => true]);
+
+    expect($user->notifications()->count())
+        ->toBe(1);
+});
+
+test('cant users with "Bills Due Tomorrow" notification disabled receive it in-app', function () {
+    seed(AvailableNotificationsSeeder::class);
+    $user = User::factory()->create();
+    $bill = Bill::factory()->create(['status' => 'pending', 'due_date' => Carbon::tomorrow()->format('Y-m-d'), 'user_id' => $user->id]);
+
+    artisan('send-emails:bills-due-tomorrow');
+    artisan('queue:work', ['--stop-when-empty' => true]);
+
+    expect($user->notifications()->count())
+        ->toBe(0);
+});
+
+test('cant users with "Bills Due Tomorrow" notification enabled receive it in-app without any bills whose due date is tomorrow', function () {
+    seed(AvailableNotificationsSeeder::class);
+    $user = User::factory()->create();
+    $bill = Bill::factory()->create(['status' => 'pending', 'due_date' => Carbon::now()->addDays(3)->format('Y-m-d'), 'user_id' => $user->id]);
+
+    artisan('send-emails:bills-due-tomorrow');
+    artisan('queue:work', ['--stop-when-empty' => true]);
+
+    expect($user->notifications()->count())
+        ->toBe(0);
+});
---
+++ tests/Feature/Notification/SendNotificationsBillsOverdueTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Carbon\Carbon;
+use App\Models\Bill;
+use App\Models\User;
+use function Pest\Laravel\seed;
+use function Pest\Laravel\artisan;
+use App\Models\AvailableNotification;
+use Database\Seeders\AvailableNotificationsSeeder;
+
+test('can users with "Bills Overdue" notification enabled receive it in-app', function () {
+    seed(AvailableNotificationsSeeder::class);
+    $billsOverdueNotificationId = AvailableNotification::where('name', 'Bills Overdue')->value('id');
+    $user = User::factory()->create();
+    $bill = Bill::factory()->create(['status' => 'overdue', 'user_id' => $user->id]);
+    $user->enabledNotifications()->attach($billsOverdueNotificationId);
+
+    artisan('send-emails:bills-overdue');
+    artisan('queue:work', ['--stop-when-empty' => true]);
+
+    expect($user->notifications()->count())
+        ->toBe(1);
+});
+
+test('cant users with "Bills Overdue" notification disabled receive it in-app', function () {
+    seed(AvailableNotificationsSeeder::class);
+    $user = User::factory()->create();
+    $bill = Bill::factory()->create(['status' => 'overdue', 'user_id' => $user->id]);
+
+    artisan('send-emails:bills-overdue');
+    artisan('queue:work', ['--stop-when-empty' => true]);
+
+    expect($user->notifications()->count())
+        ->toBe(0);
+});
+
+test('cant users with "Bills Overdue" notification enabled receive it in-app without any bills in overdue', function () {
+    seed(AvailableNotificationsSeeder::class);
+    $user = User::factory()->create();
+    $bill = Bill::factory()->create(['status' => 'pending', 'user_id' => $user->id]);
+
+    artisan('send-emails:bills-overdue');
+    artisan('queue:work', ['--stop-when-empty' => true]);
+
+    expect($user->notifications()->count())
+        ->toBe(0);
+});
