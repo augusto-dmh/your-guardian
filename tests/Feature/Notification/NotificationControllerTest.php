3b711d07ed5c2ed69505aa178cd849fc6cb6653c
= <augustodemeloh@protonmail.com>
2024-11-24 23:17:37 -0300

    feat: add authorization tests to NotificationControllerTest

--- tests/Feature/Notification/NotificationControllerTest.php
+++ tests/Feature/Notification/NotificationControllerTest.php
@@ -5,12 +5,13 @@ use App\Models\Bill;
 use App\Models\User;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\seed;
+use function Pest\Laravel\get;
 use Illuminate\Support\Facades\Log;
 use App\Notifications\BillDueTomorrowNotification;
 use Database\Seeders\AvailableNotificationsSeeder;
 // 'notification.read'
 
-test('can a notification be read after clicking on it', function () {
+test('can a user that owns a notification read it', function () {
     seed(AvailableNotificationsSeeder::class);
     $user = User::factory()->create();
     $bill = Bill::factory()->create(['user_id' => $user->id, 'status' => 'pending', 'due_date' => Carbon::tomorrow()]);
@@ -25,6 +26,36 @@ test('can a notification be read after clicking on it', function () {
         ->not
         ->toBeNull();
 });
+
+test('cant a user that doesnt own a notification read it', function () {
+    seed(AvailableNotificationsSeeder::class);
+    $owner = User::factory()->create();
+    $nonOwner = User::factory()->create();
+    $bill = Bill::factory()->create(['user_id' => $owner->id, 'status' => 'pending', 'due_date' => Carbon::tomorrow()]);
+    $notification = new BillDueTomorrowNotification($bill, $owner->language_preference);
+    $owner->notify($notification);
+    $databaseNotification = $owner->notifications->first();
+
+    $response = actingAs($nonOwner)->get(route('notification.read', $databaseNotification));
+
+    $response
+        ->assertForbidden();
+});
+
+test('cant a guest read a notification', function () {
+    seed(AvailableNotificationsSeeder::class);
+    $user = User::factory()->create();
+    $bill = Bill::factory()->create(['user_id' => $user->id, 'status' => 'pending', 'due_date' => Carbon::tomorrow()]);
+    $notification = new BillDueTomorrowNotification($bill, $user->language_preference);
+    $user->notify($notification);
+    $databaseNotification = $user->notifications->first();
+
+    $response = get(route('notification.read', $databaseNotification));
+
+    $response
+        ->assertRedirectToRoute('login');
+});
+
 
 test('is user redirected to the url of the notification after it gets read', function () {
     seed(AvailableNotificationsSeeder::class);
