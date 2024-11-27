18bf45a6c168d3d46e31ccfcf07d566e5f55030e
= <augustodemeloh@protonmail.com>
2024-11-27 00:19:12 -0300

    feat: add logic to enable in-app notifications for new users to UserObserver

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,6 +3,8 @@
 namespace App\Observers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use App\Models\NotificationChannel;
 use App\Models\AvailableNotification;
 
 class UserObserver
@@ -12,10 +14,13 @@ class UserObserver
      */
     public function created(User $user): void
     {
+        // all users created start with all notifications enabled to be pushed in-app
         $availableNotificationsIds = AvailableNotification::query()
             ->pluck('id')
             ->toArray();
+        $notificationChannelDatabase = NotificationChannel::where('slug', 'database')->first();
 
         $user->enabledNotifications()->sync($availableNotificationsIds);
+        $user->enabledNotificationChannels()->attach($notificationChannelDatabase->id);
     }
 }
