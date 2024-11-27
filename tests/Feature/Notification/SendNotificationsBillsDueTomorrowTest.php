41376f6febee19258a04e87189949480c9accccf
= <augustodemeloh@protonmail.com>
2024-11-27 12:31:26 -0300

    feat: add/modify notification-related tests to "notification channels enabling" feature

--- tests/Feature/Notification/SendNotificationsBillsDueTomorrowTest.php
+++ tests/Feature/Notification/SendNotificationsBillsDueTomorrowTest.php
@@ -5,11 +5,48 @@ use App\Models\Bill;
 use App\Models\User;
 use function Pest\Laravel\seed;
 use function Pest\Laravel\artisan;
+use App\Models\NotificationChannel;
 use App\Models\AvailableNotification;
+use Database\Seeders\NotificationChannelSeeder;
 use Database\Seeders\AvailableNotificationsSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-test('can users with "Bills Due Tomorrow" notification enabled receive it in-app', function () {
-    seed(AvailableNotificationsSeeder::class);
+test('can users with in-app notification channel and "Bills Due Tomorrow" notification enabled receive it in-app', function () {
+    seed([AvailableNotificationsSeeder::class, NotificationChannelSeeder::class]);
+    $billsDueTomorrowNotificationId = AvailableNotification::where('name', 'Bills Due Tomorrow')->value('id');
+    $inAppNotificationChannelId = NotificationChannel::where('name', 'In-app')->value('id');
+    $user = User::withoutEvents(function () {
+        return User::factory()->create();
+    });
+    $bill = Bill::factory()->create(['status' => 'pending', 'due_date' => Carbon::tomorrow()->format('Y-m-d'), 'user_id' => $user->id]);
+    $user->enabledNotifications()->attach($billsDueTomorrowNotificationId);
+    $user->enabledNotificationChannels()->attach($inAppNotificationChannelId);
+
+    artisan('send-notifications:bills-due-tomorrow');
+    artisan('queue:work', ['--stop-when-empty' => true]);
+
+    expect($user->notifications()->count())
+        ->toBe(1);
+});
+
+test('cant users with in-app notification channel disabled and "Bills Due Tomorrow" notification enabled receive it in-app', function () {
+    seed([AvailableNotificationsSeeder::class, NotificationChannelSeeder::class]);
+    $billsDueTomorrowNotificationId = AvailableNotification::where('name', 'Bills Due Tomorrow')->value('id');
+    $user = User::withoutEvents(function () {
+        return User::factory()->create();
+    });
+    $bill = Bill::factory()->create(['status' => 'pending', 'due_date' => Carbon::tomorrow()->format('Y-m-d'), 'user_id' => $user->id]);
+    $user->enabledNotifications()->attach($billsDueTomorrowNotificationId);
+
+    artisan('send-notifications:bills-overdue');
+    artisan('queue:work', ['--stop-when-empty' => true]);
+
+    expect($user->notifications()->count())
+        ->toBe(0);
+});
+
+test('cant users with in-app notification channel enabled and "Bills Due Tomorrow" notification disabled receive it in-app', function () {
+    seed([AvailableNotificationsSeeder::class, NotificationChannelSeeder::class]);
     $billsDueTomorrowNotificationId = AvailableNotification::where('name', 'Bills Due Tomorrow')->value('id');
     $user = User::withoutEvents(function () {
         return User::factory()->create();
@@ -21,29 +58,17 @@ test('can users with "Bills Due Tomorrow" notification enabled receive it in-app
     artisan('queue:work', ['--stop-when-empty' => true]);
 
     expect($user->notifications()->count())
-        ->toBe(1);
-});
-
-test('cant users with "Bills Due Tomorrow" notification disabled receive it in-app', function () {
-    seed(AvailableNotificationsSeeder::class);
-    $user = User::withoutEvents(function () {
-        return User::factory()->create();
-    });
-    $bill = Bill::factory()->create(['status' => 'pending', 'due_date' => Carbon::tomorrow()->format('Y-m-d'), 'user_id' => $user->id]);
-
-    artisan('send-notifications:bills-due-tomorrow');
-    artisan('queue:work', ['--stop-when-empty' => true]);
-
-    expect($user->notifications()->count())
         ->toBe(0);
 });
 
-test('cant users with "Bills Due Tomorrow" notification enabled receive it in-app without any bills whose due date is tomorrow', function () {
-    seed(AvailableNotificationsSeeder::class);
+test('cant users with in-app notification channel and "Bills Due Tomorrow" notification enabled receive it in-app without any bills in overdue', function () {
+    seed([AvailableNotificationsSeeder::class, NotificationChannelSeeder::class]);
+    $billsDueTomorrowNotificationId = AvailableNotification::where('name', 'Bills Due Tomorrow')->value('id');
     $user = User::withoutEvents(function () {
         return User::factory()->create();
     });
     $bill = Bill::factory()->create(['status' => 'pending', 'due_date' => Carbon::now()->addDays(3)->format('Y-m-d'), 'user_id' => $user->id]);
+    $user->enabledNotifications()->attach($billsDueTomorrowNotificationId);
 
     artisan('send-notifications:bills-due-tomorrow');
     artisan('queue:work', ['--stop-when-empty' => true]);
--- tests/Feature/Notification/SendNotificationsBillsOverdueTest.php
+++ tests/Feature/Notification/SendNotificationsBillsOverdueTest.php
@@ -5,11 +5,31 @@ use App\Models\Bill;
 use App\Models\User;
 use function Pest\Laravel\seed;
 use function Pest\Laravel\artisan;
+use App\Models\NotificationChannel;
 use App\Models\AvailableNotification;
+use Database\Seeders\NotificationChannelSeeder;
 use Database\Seeders\AvailableNotificationsSeeder;
 
-test('can users with "Bills Overdue" notification enabled receive it in-app', function () {
-    seed(AvailableNotificationsSeeder::class);
+test('can users with in-app notification channel and "Bills Overdue" notification enabled receive it in-app', function () {
+    seed([AvailableNotificationsSeeder::class, NotificationChannelSeeder::class]);
+    $billsOverdueNotificationId = AvailableNotification::where('name', 'Bills Overdue')->value('id');
+    $inAppNotificationChannelId = NotificationChannel::where('name', 'In-app')->value('id');
+    $user = User::withoutEvents(function () {
+        return User::factory()->create();
+    });
+    $bill = Bill::factory()->create(['status' => 'overdue', 'user_id' => $user->id]);
+    $user->enabledNotifications()->attach($billsOverdueNotificationId);
+    $user->enabledNotificationChannels()->attach($inAppNotificationChannelId);
+
+    artisan('send-notifications:bills-overdue');
+    artisan('queue:work', ['--stop-when-empty' => true]);
+
+    expect($user->notifications()->count())
+        ->toBe(1);
+});
+
+test('cant users with in-app notification channel disabled and "Bills Overdue" notification enabled receive it in-app', function () {
+    seed([AvailableNotificationsSeeder::class, NotificationChannelSeeder::class]);
     $billsOverdueNotificationId = AvailableNotification::where('name', 'Bills Overdue')->value('id');
     $user = User::withoutEvents(function () {
         return User::factory()->create();
@@ -21,15 +41,17 @@ test('can users with "Bills Overdue" notification enabled receive it in-app', fu
     artisan('queue:work', ['--stop-when-empty' => true]);
 
     expect($user->notifications()->count())
-        ->toBe(1);
+        ->toBe(0);
 });
 
-test('cant users with "Bills Overdue" notification disabled receive it in-app', function () {
-    seed(AvailableNotificationsSeeder::class);
+test('cant users with in-app notification channel enabled and "Bills Overdue" notification disabled receive it in-app', function () {
+    seed([AvailableNotificationsSeeder::class, NotificationChannelSeeder::class]);
+    $inAppNotificationChannelId = NotificationChannel::where('name', 'In-app')->value('id');
     $user = User::withoutEvents(function () {
         return User::factory()->create();
     });
     $bill = Bill::factory()->create(['status' => 'overdue', 'user_id' => $user->id]);
+    $user->enabledNotificationChannels()->attach($inAppNotificationChannelId);
 
     artisan('send-notifications:bills-overdue');
     artisan('queue:work', ['--stop-when-empty' => true]);
@@ -38,12 +60,14 @@ test('cant users with "Bills Overdue" notification disabled receive it in-app', 
         ->toBe(0);
 });
 
-test('cant users with "Bills Overdue" notification enabled receive it in-app without any bills in overdue', function () {
-    seed(AvailableNotificationsSeeder::class);
+test('cant users with in-app notification channel and "Bills Overdue" notification enabled receive it in-app without any bills in overdue', function () {
+    seed([AvailableNotificationsSeeder::class, NotificationChannelSeeder::class]);
+    $inAppNotificationChannelId = NotificationChannel::where('name', 'In-app')->value('id');
     $user = User::withoutEvents(function () {
         return User::factory()->create();
     });
     $bill = Bill::factory()->create(['status' => 'pending', 'user_id' => $user->id]);
+    $user->enabledNotificationChannels()->attach($inAppNotificationChannelId);
 
     artisan('send-notifications:bills-overdue');
     artisan('queue:work', ['--stop-when-empty' => true]);
