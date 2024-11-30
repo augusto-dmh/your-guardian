e029faf5bd6be59b9db5594430937373176f8176
= <augustodemeloh@protonmail.com>
2024-11-30 10:39:14 -0300

    feat: add Table component

---
+++ app/View/Components/Table.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Table extends Component
+{
+    public $instances;
+    public $modelName;
+
+    public function __construct($instances)
+    {
+        $this->instances = $instances;
+        $this->modelName = strtolower(class_basename($instances->first()));
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.table');
+    }
+}
