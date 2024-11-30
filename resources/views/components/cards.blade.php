704a39f1df0790a5c14919ca693d806e2e79528e
= <augustodemeloh@protonmail.com>
2024-11-30 10:37:50 -0300

    feat: add Cards component

---
+++ resources/views/components/cards.blade.php
@@ -0,0 +1,7 @@
+<div class="grid gap-4 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4">
+    @foreach ($instances as $instance)
+        <x-card :instance="$instance" :modelName="$modelName" >
+            <x-card-icon :instance="$instance" :modelName="$modelName" />
+        </x-card>
+    @endforeach
+</div>
